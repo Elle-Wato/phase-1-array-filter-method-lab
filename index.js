b548dcfdb62c67671e0aa0ea6007282370c8eb61
Wato Elle <watoelle@gmail.com>
2024-07-25 22:42:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    let lowerCaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase() === lowerCaseQuery
+    );
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+    let lowerCaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase().startsWith(lowerCaseQuery)
+    );
+    return matchingDrivers;
+}
+function matchName(drivers, query) {
+    let lowerCaseQuery = query.toLowerCase();
+
+    let matchingDrivers = drivers.filter(driver =>
+        driver.name.toLowerCase() === lowerCaseQuery
+    );
+    return matchingDrivers;
+}
